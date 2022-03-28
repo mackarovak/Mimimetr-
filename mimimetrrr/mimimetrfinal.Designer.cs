79e37977607647c9968453ab7fe202dbeb0177c9
Kseniya Makarova <ksenija.mackarova2014@yandex.ru>
2022-03-29 01:04:55 +0300

    fixed picture sizes

--- mimimetrrr/mimimetrfinal.Designer.cs
+++ mimimetrrr/mimimetrfinal.Designer.cs
@@ -63,6 +63,7 @@ namespace mimimetrrr
             this.pictureBox1.Location = new System.Drawing.Point(76, 131);
             this.pictureBox1.Name = "pictureBox1";
             this.pictureBox1.Size = new System.Drawing.Size(256, 221);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
             this.pictureBox1.TabIndex = 10;
             this.pictureBox1.TabStop = false;
             this.pictureBox1.Click += new System.EventHandler(this.eventAnswer);
@@ -72,6 +73,7 @@ namespace mimimetrrr
             this.pictureBox2.Location = new System.Drawing.Point(457, 131);
             this.pictureBox2.Name = "pictureBox2";
             this.pictureBox2.Size = new System.Drawing.Size(256, 221);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
             this.pictureBox2.TabIndex = 11;
             this.pictureBox2.TabStop = false;
             this.pictureBox2.Click += new System.EventHandler(this.eventAnswer);
